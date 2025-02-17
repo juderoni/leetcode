0c6db23f77e05a8a78201bcbc649bbac6e942171
Jude <70962385+juderoni@users.noreply.github.com>
2025-02-17 14:07:47 +0000

    [LeetCode Sync] Runtime - 1 ms (41.43%), Memory - 17.8 MB (69.54%)

--- my-folder/1585-the-kth-factor-of-n/solution.py
+++ my-folder/1585-the-kth-factor-of-n/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        retlist = []
-        for i in range(1,n+1):
+        k1 = 0
+        for i in range(1, n + 1):
             if n % i == 0:
-                retlist.append(i)
-        if len(retlist) >= k:
-            return retlist[k-1]
+                k1+=1
+            if k1 == k:
+                return i
         return -1
         
