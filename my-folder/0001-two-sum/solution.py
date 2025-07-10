849b541888285943035448f822b72eb0d102363c
Jude <70962385+juderoni@users.noreply.github.com>
2025-07-10 16:49:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19 MB (33.32%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,16 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashMap = {}
-        for i, vali in enumerate(nums):
-            hashMap[vali] = i
-        for i, vali in enumerate(nums):
-            comp = target - vali
-            if comp in hashMap and hashMap[comp] != i:
-                return [i, hashMap[comp]]
-
+        # two numbers that add up the target are in the list.
+        # each input one solution.
+        # no same element twice.
+        # [1,3,4,5] target = 6 rtn = [0, 3]
+        # sub list = [5, 2, 2, 1]
+        # probably going to make a second list that holds the subtraction of nums and target
+        pairDict = {}
+        
+        for i, item in enumerate(nums):
+            if target - item in pairDict:
+                return [pairDict[target-item], i]
+            pairDict[item] = i
+        
+        return [-1, -1]
+        
 
         
 
 
-            
+        
         
