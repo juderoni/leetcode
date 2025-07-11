0457ba0427370d9348744df1282d5dba0b748b5a
Jude <70962385+juderoni@users.noreply.github.com>
2025-07-11 14:49:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (81.89%)

--- my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
+++ my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -1,12 +1,19 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i, item in enumerate(haystack):
-            if item == needle[0]:
-                for j in range(0, len(needle)):
-                    if len(haystack) <= i + j or haystack[i + j] != needle[j]:
-                        break
-                    elif len(haystack) <= i + j:
-                        break
-                    elif j == len(needle) - 1 and haystack[i + j] == needle[j]:
-                        return i
+        # basically we want to keep a save of len(needle) chars as we progress through the haystack. and check whehther or not the window == needle. return first index of the window
+        windowString = ""
+        for i in range(0, len(haystack)):
+            if len(windowString) != len(needle):
+                windowString += haystack[i]
+                continue
+            print(windowString)
+            
+            if windowString == needle:
+                return i - len(needle)
+            # now we have len(windowString == len(needle))
+            windowString = windowString[1:]
+            windowString += haystack[i]
+        if windowString == needle:
+            return len(haystack) - len(needle)
         return -1
+            
