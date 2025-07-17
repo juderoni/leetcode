f0f5b02fd2172f7a4864474380b869f695dacd2b
Jude <70962385+juderoni@users.noreply.github.com>
2025-07-17 18:57:17 +0000

    [LeetCode Sync] Runtime - 6 ms (47.30%), Memory - 19 MB (30.93%)

--- my-folder/0283-move-zeroes/solution.py
+++ my-folder/0283-move-zeroes/solution.py
@@ -3,12 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        numtimespopped = 0
-        for i in range(len(nums)):
-            reali = i - numtimespopped
-            if (nums[reali] == 0):
-                nums.pop(reali)
-                nums.append(0)
-                numtimespopped += 1
-        
+        # iterate through, see non zero, send to front,
+        # keep index of where "front" is.
+        # front is not really front, just where to put nextitem
+        front = 0
+        for i, item in enumerate(nums):
+            if item == 0:
+                continue
+            else:
+                nums[front] = item
+                if front != i:
+
+                    nums[i] = 0
+                front+=1
         
